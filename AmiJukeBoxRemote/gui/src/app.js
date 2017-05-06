ff02215b695b5babd75067e8e0db8d04d279441e
Lars Sima <lars.sima@gmail.com>
2017-05-06 10:57:11 +0200

    fix names test button

--- AmiJukeBoxRemote/gui/src/app.js
+++ AmiJukeBoxRemote/gui/src/app.js
@@ -2,7 +2,7 @@ export class App {
   configureRouter(config, router) {
     config.title = 'AMI Jukebox';
     config.map([
-      { route: ['', 'import'], name: 'selections',      moduleId: 'views/import',      nav: true, title: 'Selections' },
+      { route: ['', 'selection'], name: 'selections',      moduleId: 'views/selection',      nav: true, title: 'Selections' },
     ]);
 
     this.router = router;
