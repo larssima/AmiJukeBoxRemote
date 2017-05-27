ab407b88d6b4cf41cf62d3c9aa3ec35099fee3c2
Lars Sima <lars.sima@gmail.com>
2017-05-27 17:30:17 +0200

    Open local spotify - not working

--- AmiJukeBoxRemote/Spotify/SpotifyInterface.cs
+++ AmiJukeBoxRemote/Spotify/SpotifyInterface.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -14,6 +15,7 @@ using SpotifyAPI.Local;
 using SpotifyAPI.Web.Auth;
 using SpotifyAPI.Web.Enums;
 using Json = System.Web.Helpers.Json;
+using System.Management;
 
 namespace AmiJukeBoxRemote.Spotify
 {
@@ -55,11 +57,58 @@ namespace AmiJukeBoxRemote.Spotify
             if (trackList.tracks.items.Count > 0)
             {
                 _spotify.Connect();
+                var status = _spotify.GetStatus();
+                if (status == null)
+                {
+                    if (!StartSpotify())
+                    {
+                        return false;
+                    }
+                    _spotify.Connect();
+                }
                 _spotify.ListenForEvents = true;
                 await _spotify.PlayURL(trackList.tracks.items[0].uri);
                 return true;
             }
             return false;
+        }
+
+        private bool StartSpotify()
+        {
+            try
+            {
+                var p = new Process();
+                // Redirect the output stream of the child process.
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.FileName = @"C:\Users\DiNer0-2\AppData\Roaming\Spotify\Spotify.exe";
+                //p.WindowStyle = ProcessWindowStyle.Hidden;
+                //p.CreateNoWindow = true;
+                p.Start();
+                p.WaitForExit();
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private string GetMainModuleFilepath(int processId)
+        {
+            string wmiQueryString = "SELECT ProcessId, ExecutablePath FROM Win32_Process WHERE ProcessId = " + processId;
+            using (var searcher = new ManagementObjectSearcher(wmiQueryString))
+            {
+                using (var results = searcher.Get())
+                {
+                    ManagementObject mo = results.Cast<ManagementObject>().FirstOrDefault();
+                    if (mo != null)
+                    {
+                        return (string)mo["ExecutablePath"];
+                    }
+                }
+            }
+            return null;
         }
 
         public TracksRoot GetTrackList(string artistName, string songTitle, string que)
